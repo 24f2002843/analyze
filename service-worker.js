b0dcb4df20a0e9a8cfb7d4033711082c26b31a9d
24f2002843 <24f2002843@ds.study.iitm.ac.in>
2025-10-23 04:59:31 +0530

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+// This is a stub service worker. Add functionality as needed.
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing...');
+});
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+});
